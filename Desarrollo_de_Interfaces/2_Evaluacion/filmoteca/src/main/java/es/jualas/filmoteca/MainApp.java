477bfd6bff5d354ba7c813ff206010b655bbe334
Juan Antonio Francés <3850437@alu.murciaeduca.es>
2025-01-28 19:55:38 +0100

    Últimos Cambios

--- Desarrollo_de_Interfaces/2_Evaluacion/filmoteca/src/main/java/es/jualas/filmoteca/MainApp.java
+++ Desarrollo_de_Interfaces/2_Evaluacion/filmoteca/src/main/java/es/jualas/filmoteca/MainApp.java
@@ -1,67 +1,83 @@
 package es.jualas.filmoteca;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import javafx.application.Application;
-import javafx.collections.ObservableList;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
-import javafx.scene.image.Image;
-import javafx.stage.Stage;
+        import com.fasterxml.jackson.databind.ObjectMapper;
+        import javafx.application.Application;
+        import javafx.collections.ObservableList;
+        import javafx.fxml.FXMLLoader;
+        import javafx.scene.Scene;
+        import javafx.scene.image.Image;
+        import javafx.stage.Stage;
 
-import java.io.*;
-import java.io.IOException;
-import java.util.List;
+        import java.io.File;
+        import java.io.IOException;
+        import java.io.InputStream;
+        import java.util.List;
+        import java.util.logging.Level;
+        import java.util.logging.Logger;
 
-public class MainApp extends Application {
+        public class MainApp extends Application {
 
-    @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(MainApp.class.getResource("main-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 800, 600);
-        stage.setTitle("Filmoteca");
+            private static final Logger LOGGER = Logger.getLogger(MainApp.class.getName());
 
-        // Establece el icono de la aplicación
-        Image icon = new Image(getClass().getResourceAsStream("/es/jualas/filmoteca/logo.png"));
-        stage.getIcons().add(icon);
+            @Override
+            public void start(Stage stage) throws IOException {
+                // Cargar el archivo FXML y crear la escena
+                FXMLLoader fxmlLoader = new FXMLLoader(MainApp.class.getResource("main-view.fxml"));
+                Scene scene = new Scene(fxmlLoader.load(), 800, 600);
+                stage.setTitle("Filmoteca");
 
-        stage.setScene(scene);
-        stage.show();
-    }
+                // Establecer el icono de la aplicación
+                InputStream iconStream = getClass().getResourceAsStream("/es/jualas/filmoteca/logo.png");
+                if (iconStream != null) {
+                    Image icon = new Image(iconStream);
+                    stage.getIcons().add(icon);
+                } else {
+                    LOGGER.warning("Icono no encontrado: /es/jualas/filmoteca/logo.png");
+                }
 
-    public static void main(String[] args) {
-        launch();
-    }
+                // Configurar la escena y mostrar la ventana
+                stage.setScene(scene);
+                stage.setResizable(false); // Deshabilitar la redimensión de la ventana
+                stage.show();
+            }
 
-    public void init() {
-        System.out.println("Cargando datos desde fichero datos/peliculas.json");
-        DatosFilmoteca datosFilmoteca = DatosFilmoteca.getInstancia();
-        ObjectMapper objectMapper = new ObjectMapper();
+            public static void main(String[] args) {
+                // Iniciar la aplicación
+                launch();
+            }
 
-        try {
-            List<Pelicula> lista = objectMapper.readValue(new File("datos/peliculas.json"),
-                                    objectMapper.getTypeFactory().constructCollectionType(List.class, Pelicula.class));
+            public void init() {
+                // Cargar datos desde el archivo JSON al iniciar la aplicación
+                LOGGER.info("Cargando datos desde fichero datos/peliculas.json");
+                DatosFilmoteca datosFilmoteca = DatosFilmoteca.getInstancia();
+                ObjectMapper objectMapper = new ObjectMapper();
 
-            datosFilmoteca.getListaPeliculas().setAll(lista);
-        } catch (IOException e){
-            System.out.println("ERROR al cargar los datos. La aplicación no puede iniciarse");
-            e.printStackTrace();
-            System.exit(1);
+                try {
+                    // Leer la lista de películas desde el archivo JSON
+                    List<Pelicula> lista = objectMapper.readValue(new File("datos/peliculas.json"),
+                                            objectMapper.getTypeFactory().constructCollectionType(List.class, Pelicula.class));
+
+                    // Establecer la lista de películas en DatosFilmoteca
+                    DatosFilmoteca.getListaPeliculas().setAll(lista);
+                } catch (IOException e) {
+                    // Manejar errores de lectura del archivo JSON
+                    LOGGER.log(Level.SEVERE, "ERROR al cargar los datos. La aplicación no puede iniciarse", e);
+                    System.exit(1);
+                }
+            }
+
+            public void stop() {
+                // Guardar los datos en el archivo JSON al cerrar la aplicación
+                ObservableList<Pelicula> listaPeliculas = DatosFilmoteca.getInstancia().getListaPeliculas();
+                LOGGER.info(listaPeliculas.toString());
+                ObjectMapper objectMapper = new ObjectMapper();
+
+                try {
+                    // Escribir la lista de películas en el archivo JSON
+                    objectMapper.writeValue(new File("datos/peliculas.json"), listaPeliculas);
+                } catch (IOException e) {
+                    // Manejar errores de escritura del archivo JSON
+                    LOGGER.log(Level.SEVERE, "ERROR no se ha podido guardar los datos de la aplicación", e);
+                }
+            }
         }
-
-
-    }
-
-    public void stop() {
-        ObservableList<Pelicula> listaPeliculas = DatosFilmoteca.getInstancia().getListaPeliculas();
-        System.out.println(listaPeliculas);
-        ObjectMapper objectMapper = new ObjectMapper();
-
-        try {
-            objectMapper.writeValue(new File("datos/peliculas.json"),listaPeliculas);
-        }catch (IOException e) {
-            System.out.println("ERROR no se ha podido guardar los datos de la aplicación");
-            e.printStackTrace();
-        }
-
-    }
-}
--- Desarrollo_de_Interfaces/2_Evaluacion/filmoteca/src/main/java/module-info.java
+++ Desarrollo_de_Interfaces/2_Evaluacion/filmoteca/src/main/java/module-info.java
@@ -3,6 +3,7 @@ module es.jualas.filmoteca {
     requires javafx.fxml;
     requires com.fasterxml.jackson.databind;
     requires javafx.base;
+    requires java.logging;
 
     opens es.jualas.filmoteca to javafx.fxml, com.fasterxml.jackson.databind;
     exports es.jualas.filmoteca;
